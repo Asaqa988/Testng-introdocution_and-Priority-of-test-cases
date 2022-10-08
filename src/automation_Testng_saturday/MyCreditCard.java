0076bf5470b393b079e76ff3b978d2f89ee97ae3
asaqa988 <asaqa001@gmail.com>
2022-10-08 13:58:52 +0300

    update

---
+++ src/automation_Testng_saturday/MyCreditCard.java
@@ -0,0 +1,16 @@
+package automation_Testng_saturday;
+
+import org.testng.annotations.Test;
+
+public class MyCreditCard {
+	
+	@Test
+	public void myCreditOne() {
+		
+	}
+
+	@Test
+	public void myCreditTwo() {
+		
+	}
+}
---
+++ src/automation_Testng_saturday/MyVoucher.java
@@ -0,0 +1,23 @@
+package automation_Testng_saturday;
+
+import org.testng.annotations.Test;
+
+public class MyVoucher {
+	
+	@Test 
+	public void myvoucherone() {
+		
+		
+	}
+	
+	@Test 
+	public void myvouchertwo() {
+		
+	}
+	@Test 
+	public void myvoucherthree() {
+		
+	}
+
+
+}
